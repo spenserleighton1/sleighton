6dfe8ef96ad4df9ef90a6a9e9bb9f7fb18f69edd
Spenser Leighton <spenser.leighton@gmail.com>
2019-07-03 12:44:24 -0600

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
+// import React from 'react';
+// import App from './App.js';
+// import { shallow } from 'enzyme';
+
+// describe('App', () => {
+//   let wrapper = shallow(<App/>);
+//   it('should match the snapShot', () => {
+//     console.log('wrap that shit')
+//     expect(wrapper).toMatchSnapshot()
+//   })
+// })
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
