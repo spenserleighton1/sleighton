72ef9211a00864dad0dd3dd55e10ce4a08dbf614
Spenser Leighton <spenser.leighton@gmail.com>
2019-07-11 21:03:40 -0600

    This might be a good place to revert

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import github from '../../Assets/github.svg'
 import instagram from '../../Assets/instagram-logo.svg'
 import linkedin from '../../Assets/linkedin-logo-button.svg'
 import twitter from '../../Assets/twitter-logo-button.svg'
+import mail from '../../Assets/closed-envelope-circle.svg'
 import './Header.scss';
 
 const Header = (props) => {
@@ -13,6 +14,7 @@ const Header = (props) => {
                 <li><a href="https://instagram.com/spennifer_love_hewitt_/"><img src={ instagram } alt="instagram"/></a></li>
                 <li><a href="https://www.linkedin.com/in/spenser-leighton/"><img src={ linkedin } alt="linkedin"/></a></li>
                 <li><a href="https://twitter.com/takemenorth"><img src={ twitter } alt="twitter"/></a></li>
+                <li><a href="mailto:spenser.leighton@gmail.com"><img src={ mail } alt="mail"/></a></li>
                 <li><div id="hambmenu" onClick={ () => props.toggleMenu() } className={ props.menuStatus }><span></span><span></span><span></span><span></span></div></li>
             </ul>
         </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -14,13 +14,19 @@ class Home extends Component {
         let homeClass = this.props.menuStatus + ' home';
         return(
             <div className={ homeClass }>
-                <div className="header">
+
                     <div className="name-container">
                         <div className="name-bg"></div>
                         <h1 className="name-fg">Spenser Leighton</h1>
                         <h1 className="name-sh">Software Developer</h1>
                     </div>
-                </div>
+
+                    <div className="about-container">
+                        <div className="about-bg"></div>
+                        <h2 className="about-fg">Hello, I'm a<span className="inside-joke">n</span> software developer based in Denver CO. </h2>
+                        <h2 className="about-sh">She can stay afloat with the first four compartments breached but not five. Not five. She goes down by the head, the water will spill over the bulkheads, from one to the next back and back there's no stopping it.</h2>
+                    </div>
+
             </div>
         )
     }
